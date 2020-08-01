d13466d40dc8ec7ad2b20b2db9023c146dcda2cb
rad-0623 <ichi623110@gmail.com>
2020-08-01 23:41:32 +0900

    スターと画面へいけた

---
+++ 02_Unity_File/00_パズル基礎作成テスト/paz copy/Assets/src/Battle/ChangeBotun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeBotun : MonoBehaviour
+{
+  public GameObject controlTrun;
+  public ControlTrun control_turn;
+  void Start()
+  {
+    control_turn = controlTrun.GetComponent<ControlTrun>();
+  }
+  void OnMouseDown(){
+    control_turn.changeTurn();
+    Debug.Log(control_turn.getTurn());
+  }
+}
---
+++ 02_Unity_File/00_パズル基礎作成テスト/paz copy/Assets/src/GameOver/Endlesenemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endlesenemy : MonoBehaviour
+{
+  public GameObject enemy1;
+  float span = 0.2f;
+  float del = 0;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+      this.del += Time.deltaTime;
+      if(this.del > this.span){
+        this.del = 0;
+        GameObject go = Instantiate(enemy1) as GameObject;
+        int x = Random.Range(-30,30);
+        int z = Random.Range(10,40);
+        int sp = Random.Range(0,360);
+        go.transform.position = new Vector3(x,40,z);
+        go.transform.Rotate(sp,sp,sp);
+      }
+    }
+}
