697e71b5d7c3e558472e56b68fa2978aba9fedd3
吉田　航 <wataruponnta@gmail.com>
2020-10-30 21:23:18 +0900

    8000comit

---
+++ Talk/Assets/Text.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+  public string[,] texts = {
+    {"主人公","これはデモテキスト、主人公です。"},
+    {"主人公","これって何のために作ったの?"},
+    {"おもしれー女","各シーンの基本を知るためです。"},
+    {"主人公","それで、何となくわかったの？"},
+    {"おもしれー女","全くわかりませんでした！"},
+    {"主人公","まあ、そんなもんだよね〜"},
+    {"おもしれー女","とりあえず次のシーンに行っちゃおう！"}
+  };
+}
